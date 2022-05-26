28d7361bc705a2088f393a6616f41878d0f14987
zequee <ezequielpereda@gmail.com>
2022-05-26 17:27:53 -0300

    add react-router-dom for route the pages in the url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetupsPage";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
-import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./utils/constants";
+import {
+  ALL_MEETUP_PAGE,
+} from "./utils/constants";
 
 import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
@@ -12,19 +14,15 @@ function App() {
   const [page, setPage] = useState(ALL_MEETUP_PAGE);
 
   function getCurrentPageComponent() {
-    let currentPageComponent = <AllMeetupsPage />;
-    switch (page) {
-      case FAVORITES_PAGE:
-        currentPageComponent = <FavoritesPage />;
-        break;
-      case NEW_MEETUP_PAGE:
-        currentPageComponent = <NewMeetupsPage />;
-        break;
-      default:
-        currentPageComponent = <AllMeetupsPage />;
-    }
-
-    return currentPageComponent;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={AllMeetupsPage} />
+          <Route path="/favorites" exact component={FavoritesPage} />
+          <Route path="/newMeetup" exact component={NewMeetupsPage} />
+        </Switch>
+      </Router>
+    );
   }
 
   return (
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -15,18 +15,18 @@ export default function MainNavigation({ setPage }) {
         <nav>
           <ul>
             <li>
-              <a href="#" onClick={() => setPage(ALL_MEETUP_PAGE)}>
+              <a href="/" onClick={() => setPage(ALL_MEETUP_PAGE)}>
                 All Meetups
               </a>
             </li>
 
             <li>
-              <a href="#" onClick={() => setPage(NEW_MEETUP_PAGE)}>
+              <a href="/newMeetup" onClick={() => setPage(NEW_MEETUP_PAGE)}>
                 Add New Meetup
               </a>
             </li>
             <li>
-              <a href="#" onClick={() => setPage(FAVORITES_PAGE)}>
+              <a href="/favorites" onClick={() => setPage(FAVORITES_PAGE)}>
                 My Favorites
                 <span className={classes.badge}>{0}</span>
               </a>
